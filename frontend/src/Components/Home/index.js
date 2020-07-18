98edcd9c25e1abbb2b1d838015986704402556f7
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-07-17 22:15:34 -0300

    Iniíco da Caixa de Login

--- frontend/src/Components/Home/index.js
+++ frontend/src/Components/Home/index.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import '../global.css';
+import './index.css';
 
 export default class Home extends Component {
     render() {
         return (
             <div class="content">
                 <h1 class="title">Activty Boards</h1>
+                <p class="description">Descrição</p>
                 <form>
-                    <label>Username</label>
-                    <input type="text"/>
-                    <label>Password</label>
-                    <input type="password"/>
+                    <div class="login">
+                        <label>Username</label>
+                        <input type="text"/>
+                        <label>Password</label>
+                        <input type="password"/>
+                        <button class="primary-btn">Log in</button>
+                    </div>
                 </form>
-            </div>
+        </div>
         )
     }
 }
