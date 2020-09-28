c04dc532d0c018d1ebf69ccdd86c2c4fd27f7d41
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2020-09-27 21:15:51 -0300

    Início da criação de rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,22 @@
-import express from 'express';
+const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/teste', (request, response) => {
-   response.send("foi bro");
+// Rota de registro do usuário
+routes.post('/api/register', (req, res) => {
+   const { email, firstname, lastname, password } = req.body;
+   res.send("Usuário registrado");
 });
 
-export default routes;
+// Rota de login do usuário
+routes.post('/api/authenticate', (req, res) => {
+   res.send("Usuário logado");
+});
+
+// Rota de pegar informações do usuário
+routes.get('/api/me', (req, res) => {
+   res.send("Informações do usuário");
+});
+
+module.exports = routes;
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
-import express from 'express';
-import routes from './routes';
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
 
-app.use(express.json());
+// entender informações json
+app.use(bodyParser.json())
+// entender parâmetros de url
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use(routes);
 
 app.listen(3333);
